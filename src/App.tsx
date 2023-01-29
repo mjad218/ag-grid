3cbf6a08e8ffabd8e60a520cde85e6fb06e49ab9
Mohamed Gad <59421219+mjad218@users.noreply.github.com>
2023-01-29 14:52:47 +0200

    remove selected rows

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { RefObject, useRef, useState } from "react";
 import "./App.css";
 import { AgGridReact } from "ag-grid-react";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-import { ColDef } from "ag-grid-community";
+import { ColDef, GridApi } from "ag-grid-community";
 import { getFakeRowData, RowData } from "./helpers";
 
 function App() {
   const [rowData, setRowData] = useState<RowData[]>([]);
+  const tableRef = useRef<any>(null);
 
-  //: ( id,name,last_name,status, actions )
   const [columnDefs] = useState([
     { field: "id" },
     { field: "lastName" },
@@ -18,6 +18,14 @@ function App() {
     // { field: "actions" },
   ]);
 
+  const removeSelectedRows = () => {
+    const selectedNodes = (tableRef.current.api as GridApi).getSelectedNodes();
+    const newRows = rowData.filter((row) =>
+      selectedNodes.every((node) => node.data.id !== row.id)
+    );
+
+    setRowData(newRows);
+  };
   const onInsertRandom = () => {
     const row = getFakeRowData();
     setRowData((prev) => [...prev, row]);
@@ -29,8 +37,11 @@ function App() {
   return (
     <div className="ag-theme-alpine" style={{ height: 400 }}>
       <button onClick={onInsertRandom}>Insert Random</button>
+      <button onClick={removeSelectedRows}>Remove Selected</button>
       <AgGridReact
+        ref={tableRef}
         rowData={rowData}
+        rowSelection="multiple"
         columnDefs={columnDefs}
         getRowId={getRowId}
       ></AgGridReact>
